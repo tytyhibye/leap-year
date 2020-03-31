8ff8e20e594b3b486ed74fc85e9cd0154b0e0453
Tyler Bates <Fondlytyler@gmail.com>
2020-03-31 15:24:09 -0700

    refactor if/else if to avoid redundancy.

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,7 @@ $(document).ready(function() {
   $("form#leap-year").submit(function(event) {
     event.preventDefault();
     var leapYear = function(year) {
-      if (year % 100 === 0) {
-        return false;
-      } else if (year % 4 === 0) {
+      if ((year % 4 === 0) && (year % 100 !== 0)) {
         return true;
       } else {
       return false;
