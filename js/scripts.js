ff1ba90ec18957d8206ad68d5cc65df5e2b2c1fa
Tyler Bates <Fondlytyler@gmail.com>
2020-03-31 15:12:29 -0700

    pass our first spec of returning false if a year entered is not a leap year.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,12 @@
 $(document).ready(function() {
   $("form#leap-year").submit(function(event) {
-  event.preventDefault();
-  var year = parseInt($("input#year").val());
-  var result = leapYear(year);
-  $("#result").text(result);
+    event.preventDefault();
+    var leapYear = function(year) {
+      return false;
+    };
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+    
+    $("#result").text(result);
   });
 });
