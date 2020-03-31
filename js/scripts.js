7dbde491ffc569f321dd513481ec688e6355f11e
Tyler Bates <Fondlytyler@gmail.com>
2020-03-31 15:25:48 -0700

    add final spec of checking if input is divisible by 400.

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   $("form#leap-year").submit(function(event) {
     event.preventDefault();
     var leapYear = function(year) {
-      if ((year % 4 === 0) && (year % 100 !== 0)) {
+      if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
         return true;
       } else {
       return false;
