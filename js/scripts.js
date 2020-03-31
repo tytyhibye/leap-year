67c8502d07a1d9475d6ead3c0d144153732a4490
Tyler Bates <Fondlytyler@gmail.com>
2020-03-31 15:15:49 -0700

    confirm second spec complete of returning true if input is divisible by 4.

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,11 @@ $(document).ready(function() {
   $("form#leap-year").submit(function(event) {
     event.preventDefault();
     var leapYear = function(year) {
+      if (year % 4 === 0) {
+        return true;
+      } else {
       return false;
+      }
     };
     var year = parseInt($("input#year").val());
     var result = leapYear(year);
